9af1375d11deb0c13c9353ff3f62d1fee483c617
mariam george <mariam0107@gmail.com>
2025-07-01 12:04:02 +0200

    Added properties for discovery

--- src/main/java/com/microserviceproject/question_service/QuestionServiceApplication.java
+++ src/main/java/com/microserviceproject/question_service/QuestionServiceApplication.java
@@ -2,8 +2,10 @@ package com.microserviceproject.question_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
